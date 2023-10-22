7c4218f8e89aa3a1da840945045d674b49a574f7
Christopher Obando <chrisjobando@gmail.com>
2023-10-21 22:26:51 -0700

    add website deployment script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
+  site: 'https://chrisobando.dev',
+  integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: 'nord'
